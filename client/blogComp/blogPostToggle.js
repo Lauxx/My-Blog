bc0c7309fb8dee43e093c3a65e47ca40adfaf4bc
Lauxx <lm.nichols@hotmail.com>
2016-04-05 21:37:06 -0600

    now able to toggle to blog list from blog form

--- client/blogComp/blogPostToggle.js
+++ client/blogComp/blogPostToggle.js
@@ -6,7 +6,12 @@ var BlogPostToggle = React.createClass({
 		return (
 			<div className="container col-xs-1 col-xs-offset-10">
 				<div className="btn-group" data-toggle="buttons">
-					<a className="btn btn-default" onClick={ this.props.toggleActiveComp.bind(null, 'blogPostForm') }>Post a Blog</a>
+					<a className="btn btn-default" 
+					onClick={ this.props.toggleActiveComp.bind(null, 'blogPostForm') }>
+					Post a Blog</a>
+					<a className="btn btn-default" 
+					onClick={ this.props.toggleActiveComp.bind(null, 'blogList') }>
+					Blog List</a>
 				</div>
 			</div>
 			)
