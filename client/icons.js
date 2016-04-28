d13c581acb37420209f77d6a5474000a548d2599
Lauxx <lm.nichols@hotmail.com>
2016-04-28 15:06:43 -0600

    fixing dead space on right side due to icons being in a container

--- client/icons.js
+++ client/icons.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Icons = React.createClass({
 	render: function(){
 		return(
-			<div className="container">
+			<div className="">
 				<a className="" href="https://twitter.com/coding_lotus"><img src="img/twitter.png" height="60px" width="60px"/></a>
 				<a className="" href="https://linkedin.com/in/laurennichols11"><img src="img/linkedin.png" height="60px" width="60px"/></a>
 				<a className="" href="https://github.com/lauxx"><img src="img/github.png" height="60px" width="60px"/></a>
