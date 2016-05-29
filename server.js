832a1754251b5e0c0968c961c6bd86cdefa49079
Lauxx <lm.nichols@hotmail.com>
2016-05-29 12:37:14 -0600

    wrong database connected

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ var options = {
 server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };  
-var mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/hugApp";
+var mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/blog";
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
-console.log(mongooseUri);
+//console.log(mongooseUri);
 
 mongoose.connect(mongooseUri, options, function(err, data){
   if(err){
